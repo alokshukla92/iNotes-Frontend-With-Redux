14a08b4dc1328cdf7c12e6aa745009dc9674451d
Alok Shukla <alokshukla994@gmail.com>
2024-08-11 01:45:22 +0530

    feat: Remove About Component

--- src/components/utils/Navbar.js
+++ src/components/utils/Navbar.js
@@ -42,16 +42,6 @@ const Navbar = () => {
                 Home
               </Link>
             </li>
-            <li className="nav-item">
-              <Link
-                className={`nav-link ${
-                  location.pathname === "/about" ? "active" : ""
-                }`}
-                to="/about"
-              >
-                About
-              </Link>
-            </li>
           </ul>
 
           {!localStorage.getItem("token")?<form className="form-inline my-2 my-lg-0 mx-2">
