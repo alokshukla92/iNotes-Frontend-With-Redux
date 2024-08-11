e8bd68aa906d4e3592090fb46ebf3729434a60d0
Alok Shukla <alokshukla994@gmail.com>
2024-08-11 14:01:46 +0530

    feat: Update TODO Functionallity

--- src/components/Todos/TodoItem.js
+++ src/components/Todos/TodoItem.js
@@ -1,7 +1,7 @@
 // src/components/Todos/TodoItem.js
 import axios from 'axios';
 
-const TodoItem = ({ todo, onDelete, onEdit }) => {
+const TodoItem = ({ todo, onDelete, onUpdate }) => {
   const iconStyle = {
     fontSize: "1.2rem", 
     marginLeft: "0.5rem", 
@@ -17,24 +17,12 @@ const TodoItem = ({ todo, onDelete, onEdit }) => {
     }
   };
 
-  const handleEdit = async () => {
-    // TODO: Handle Later
-    console.log("Edit Cicked");
-    
-    // try {
-    //   await axios.delete(`/api/notes/updatenote/${todo._id}`);
-    //   onEdit(todo._id);
-    // } catch (error) {
-    //   console.error('Failed to delete todo:', error);
-    // }
-  };
-
   return (
     <div className="card my-3">
       <div className="card-body">
         <div className="d-flex align-items-center">
           <h5 className="card-title mb-0 me-2">{todo.title}</h5>
-          <i className="bi bi-pencil-square" onClick={handleEdit} style={iconStyle}></i>
+          <i className="bi bi-pencil-square" onClick={()=>{onUpdate(todo)}} style={iconStyle}></i>
           <i className="bi bi-trash3" onClick={handleDelete} style={iconStyle}></i>
         </div>
         <p className="card-text">{todo.description}</p>
--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -1,4 +1,3 @@
-// src/components/Todos/TodoList.js
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import TodoItem from './TodoItem';
@@ -6,6 +5,9 @@ import TodoForm from './TodoForm';
 
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
+  const [currentTodo, setCurrentTodo] = useState(null);
+  const [editTitle, setEditTitle] = useState('');
+  const [editDescription, setEditDescription] = useState('');
 
   useEffect(() => {
     const fetchTodos = async () => {
@@ -28,32 +30,129 @@ const TodoList = () => {
     setTodos(todos.filter(todo => todo._id !== id));
   };
 
-  const handleEditTodo = (id, title, description) => {
-    const newNotes = todos.map(item => {
-      if (item._id === id) {
-        return {
-          ...item,
-          title: title,
-          description: description
-        };
-      }
-      return item;
-    });
-    setTodos(newNotes)
+  const handleEditClick = (todo) => {
+    setCurrentTodo(todo);
+    setEditTitle(todo.title);
+    setEditDescription(todo.description);
+    const editModal = new window.bootstrap.Modal(document.getElementById('editTodoModal'));
+    editModal.show();
   };
 
+  const handleEditTodo = async () => {
+    if (currentTodo) {
+      const updatedTodo = {
+        ...currentTodo,
+        title: editTitle,
+        description: editDescription
+      };
+  
+      try {
+        // Make the API call to update the todo item
+        const response = await axios.put(`/api/notes/updatenote/${updatedTodo._id}`, {
+          title: updatedTodo.title,
+          description: updatedTodo.description
+        });
+        
+        // Update state only if the API call is successful
+        setTodos(todos.map(todo => (todo._id === updatedTodo._id ? updatedTodo : todo)));
+        
+        // Hide the modal
+        const editModal = window.bootstrap.Modal.getInstance(document.getElementById('editTodoModal'));
+        if (editModal) {
+          editModal.hide();
+        }
+        
+        console.log('Todo updated:', response.data);
+      } catch (error) {
+        console.error('Failed to update todo:', error);
+      }
+    }
+  };
+  
 
   return (
     <div className="container my-3">
       <TodoForm onAddTodo={handleAddTodo} />
-      <hr className="my-4" /> {/* Add a horizontal line for separation */}
+      <hr className="my-4" />
       <h3>My Notes</h3>
       <div className="row my-3 mx-3">
         {todos.map(todo => (
           <div key={todo._id} className="col-md-3">
-          <TodoItem key={todo._id} todo={todo} onDelete={handleDeleteTodo} onEdit={handleEditTodo} />
+            <TodoItem
+              todo={todo}
+              onDelete={handleDeleteTodo}
+              onUpdate={() => handleEditClick(todo)}
+            />
           </div>
         ))}
+      </div>
+
+      {/* Bootstrap Modal for editing todos */}
+      <div
+        className="modal fade"
+        id="editTodoModal"
+        tabIndex="-1"
+        aria-labelledby="editTodoModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="editTodoModalLabel">
+                Edit Note
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="editTitle" className="form-label">
+                    Title
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="editTitle"
+                    value={editTitle}
+                    onChange={(e) => setEditTitle(e.target.value)}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="editDescription" className="form-label">
+                    Description
+                  </label>
+                  <textarea
+                    id="editDescription"
+                    className="form-control"
+                    value={editDescription}
+                    onChange={(e) => setEditDescription(e.target.value)}
+                  />
+                </div>
+              </form>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={handleEditTodo}
+              >
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
